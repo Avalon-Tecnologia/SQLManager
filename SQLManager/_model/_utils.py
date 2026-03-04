7240e570939a73876c53a87a73959c1bc789bfdc
nicozsd <Nicolas.damasio@uol.com.br>
2026-03-04 20:36:01 -0300

    __init__ hf 03 - Utils import fix

--- SQLManager/_model/_utils.py
+++ SQLManager/_model/_utils.py
@@ -1,6 +1,7 @@
 ''' [BEGINs CODE] Project: SQLManager Version 4.0 / issue: #6 / made by: Nicolas Santos / created: 26/02/2026 '''
 
-from . import *
+import importlib
+
 
 class utils:
     '''Funções utilitárias'''
@@ -8,7 +9,9 @@ class utils:
     @staticmethod
     def _clear_init_files_pre_import():
         '''Limpa arquivos __init__.py antes de importar'''        
-        
+        pkg = importlib.import_module(__package__)
+        init_files = getattr(pkg, 'init_files', [])
+
         for init_file in init_files:
             if init_file.exists():
                 with open(init_file, 'w', encoding='utf-8') as f:
@@ -17,6 +20,12 @@ class utils:
 
     @staticmethod
     def stepInfo(_step: str, _desc: str):        
+        pkg = importlib.import_module(__package__)
+        SystemController = getattr(pkg, 'SystemController', None)
+        if SystemController is None:
+            print(f"\n[{_step}] {_desc}...")
+            return
+
         print(f"\n[{SystemController().custom_text(_step, 'cyan', is_bold=True)}] {_desc}...")
 
 ''' [END CODE] Project: SQLManager Version 4.0 / issue: #6 / made by: Nicolas Santos / created: 26/02/2026 '''
